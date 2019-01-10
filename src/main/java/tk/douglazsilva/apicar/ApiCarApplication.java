8c6a130195073f22a94f0ff1febb82518972736a
douglazsilva <fdcsone@hotmail.com>
2019-01-10 14:43:56 -0200

    iniciando projeto

---
+++ src/main/java/tk/douglazsilva/apicar/ApiCarApplication.java
@@ -0,0 +1,14 @@
+package tk.douglazsilva.apicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/tk/douglazsilva/apicar/ApiCarApplicationTests.java
@@ -0,0 +1,17 @@
+package tk.douglazsilva.apicar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
