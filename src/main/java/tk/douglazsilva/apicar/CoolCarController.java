0d07faa9940cc06bebd22592e9e38a1ebc411d65
douglazsilva <fdcsone@hotmail.com>
2019-01-10 18:55:14 -0200

    habilitando CORS

--- src/main/java/tk/douglazsilva/apicar/CoolCarController.java
+++ src/main/java/tk/douglazsilva/apicar/CoolCarController.java
@@ -3,6 +3,7 @@ package tk.douglazsilva.apicar;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class CoolCarController {
 	}
 	
 	@GetMapping("/cool-cars")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Collection<Car> coolCars(){
 		return repository.findAll().stream().filter(this::isCool)
 				.collect(Collectors.toList());
