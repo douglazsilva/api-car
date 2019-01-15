c207b0303d297668a0f2f086c3fb87347a3fbe35
douglazsilva <fdcsone@hotmail.com>
2019-01-15 13:15:31 -0200

    corrigindo listagem de carros

--- src/main/java/tk/douglazsilva/apicar/ApiCarApplication.java
+++ src/main/java/tk/douglazsilva/apicar/ApiCarApplication.java
@@ -1,12 +1,18 @@
 package tk.douglazsilva.apicar;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableResourceServer
 @SpringBootApplication
@@ -28,6 +34,21 @@ public class ApiCarApplication {
 			});
 			//repository.findAll().forEach(System.out::println);
 		};
+	}
+	
+	@Bean
+    @SuppressWarnings("unchecked")
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
 	}
 
 }
